632b8c01990e1c3d7b009ef7b74e2fe16cb0d8ff
Ricky Mendez <21ricky@gmail.com>
2016-09-18 15:30:12 -0600

    Initial commit

---
+++ DMLedgerASP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DMLedgerASP.Startup))]
+namespace DMLedgerASP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
